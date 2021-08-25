e6c55e5f465de83c81f66665992d9ae7b77cf41c
XFegan <xavierslf11@gmail.com>
2021-08-24 23:55:32 -0400

    test

---
+++ db/schema.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+-- using the database
+USE employee_tracker;
+
+-- Deleting if the tables already exists
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+-- Creating a database
+
+
+-- creating department table
+CREATE TABLE department (
+
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+ 
+);
+
+-- Creating role table
+CREATE TABLE role (
+
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+-- Creating employee table
+CREATE TABLE employee (
+
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+FOREIGN KEY (role_id) REFERENCES role (id),
+manager_id INTEGER NULL,
+FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
